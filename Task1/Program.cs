b7274463d3582fbba4adce70701a759311be7f96
Alex Artsy <67740905+AlexArtsy@users.noreply.github.com>
2023-04-02 02:13:18 +0400

    Add Task1

---
+++ Task1/Program.cs
@@ -0,0 +1,46 @@
+﻿namespace Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int getNextValue(int n, int m, int currentValue) 
+            {
+                for(int i = 1; i < m; i++)
+                {
+                    if(currentValue == n)
+                    {
+                        currentValue = 1;
+                    }
+                    else
+                    {
+                        currentValue++;
+                    }
+                }
+                return currentValue;
+            }
+
+            int n = Int32.Parse(args[0]);
+            int m = Int32.Parse(args[1]);
+            List<int> list = new List<int> (){ 1 };
+            int currentValue = 0;
+
+            if(n == 0)
+            {
+                Console.WriteLine("Ошибка: по условию задания n >= 1");
+                return;
+            }
+
+            while(currentValue != 1)
+            {
+                currentValue = getNextValue(n, m, list.Last());
+                if(currentValue != 1)
+                {
+                    list.Add(currentValue);
+                }
+            }
+            var result = String.Join("", list);
+            Console.WriteLine(result);
+        }
+    }
+}
